990a9facb296ba6a0de5f6bd5f3f773d8317a22b
Igor.Kramar <ikramar@t1-consulting.ru>
2023-05-12 16:28:03 +0300

    add useScrollToBottom

---
+++ src/hooks/useScrollToBottom.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+export function useScrollToBottom(callback: () => void): void {
+  useEffect(() => {
+    function handleScroll(): void {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+
+      // Проверяем, достигнута ли нижняя граница экрана
+      if (scrollTop + clientHeight >= scrollHeight) {
+        callback();
+      }
+    }
+
+    // Добавляем обработчик события прокрутки
+    window.addEventListener('scroll', handleScroll);
+
+    // Удаляем обработчик события прокрутки при размонтировании компонента
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [callback]);
+}
